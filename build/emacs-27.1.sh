8a8d68b38656e2fcd41d3207626e8092a72db11f
Takaaki ISHIKAWA <takaxp@ieee.org>
2020-02-09 11:05:23 +0900

    Try github workspace

--- build/emacs-27.1.sh
+++ build/emacs-27.1.sh
@@ -5,7 +5,17 @@ MACSDK=`xcrun --show-sdk-path`
 export LIBXML2_CFLAGS="-I${MACSDK}/usr/include/libxml2"
 export LIBXML2_LIBS="-lxml2"
 
-cd ~/Desktop
+export WORKING_DIR="~/Desktop"
+while getopts d: opt
+do
+    case ${opt} in
+        d)
+            WORKING_DIR=${OPTARG}
+            ;;
+    esac
+done
+
+cd ${WORKING_DIR}
 git clone git://git.sv.gnu.org/emacs.git
 git clone --depth 1 https://github.com/takaxp/ns-inline-patch.git
 
