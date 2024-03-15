46c7343b80cceb530d3ea5a872f607d5781ff8b3
Takaaki ISHIKAWA <takaxp@ieee.org>
2024-03-15 15:36:35 +0900

    A codesign test

--- release/portable-emacs.sh
+++ release/portable-emacs.sh
@@ -230,16 +230,10 @@ fi
 
 # Codesign
 cd ${APPDIR}
-ls
-SIGNID=`security find-identity -v`
-echo ${SIGNID}
+# security find-identity -v
 DEVELOPERID='Developer ID Application: Takaaki Ishikawa (H2PH8KNN3H)'
-echo ${DEVELOPERID}
-echo "1"
-codesign --sign "${DEVELOPERID}" --force --verbose ${APPDIR}/Emacs.app
-echo "2"
-codesign --verify --sign "Developer ID Application: Takaaki Ishikawa (H2PH8KNN3H)" --deep --force --verbose --option runtime --timestamp Emacs.app
-codesign -dv ./Emacs.app
+codesign --verify --sign "${DEVELOPERID}" --deep --force --verbose --option runtime --timestamp ./Emacs.app
+# codesign -dv ./Emacs.app
 RESULT=`pkgutil --check-signature ./Emacs.app | grep "no sign"`
 if [ "${RESULT}" ]; then
     exit 1
