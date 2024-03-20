e32aa70118f4bb818b08332bb5cf6185fadf6307
Takaaki ISHIKAWA <takaxp@ieee.org>
2024-03-20 11:27:00 +0900

    A build test

--- build/setup.sh
+++ build/setup.sh
@@ -3,6 +3,15 @@
 function setup_homebrew () {
     # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+    if [ $(uname -m) = "arm64" ]; then
+        (echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ${HOME}/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+        (echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ${HOME}/.zprofile
+        eval "$(/usr/local/bin/brew shellenv)"
+    fi
+    brew shellenv
 }
 
 function install_deps () {
