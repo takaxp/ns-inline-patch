d63dd0a10a600f17389f5a7daebe961eb4084e0e
Takaaki ISHIKAWA <takaxp@ieee.org>
2024-03-30 12:32:00 +0900

    Fix script for x86_64

--- release/portable-emacs.sh
+++ release/portable-emacs.sh
@@ -2,6 +2,7 @@
 
 # 10.14.* Mojave, 10.15.* Catalina
 # 11.* Big Sur, 12.* Monterey, 13.* Ventura, 14.* Sonoma
+ARCH=$(uname -m)
 PRODUCTVERSION=`sw_vers -ProductVersion`
 NETTLEVERSION="3.9.1" # "3.7.3", "3.8.1"
 if [ ${PRODUCTVERSION%%.*} -lt 11 ]; then
@@ -92,7 +93,9 @@ if [ $NATIVECOMPILE = true ]; then
     cp ${HOMEBREWDIR}/opt/libmpc/lib/libmpc.3.dylib lib
     cp ${HOMEBREWDIR}/opt/mpfr/lib/libmpfr.6.dylib lib
     # /usr/lib/libz.1.dylib
-#    cp ${HOMEBREWDIR}/opt/zstd/lib/libzstd.1.dylib lib
+    if [ "${ARCH}" = "x86_64" ]; then
+        cp ${HOMEBREWDIR}/opt/zstd/lib/libzstd.1.dylib lib
+    fi
 #    cp ${HOMEBREWDIR}/opt/gcc/lib/gcc/current/libgcc_s.1.1.dylib lib
 fi
 
@@ -118,7 +121,9 @@ if [ $NATIVECOMPILE = true ]; then
     install_name_tool -id "homebrew:libmpc/lib/libmpc.3.dylib" lib/libmpc.3.dylib
     install_name_tool -id "homebrew:mpfr/lib/libmpfr.6.dylib" lib/libmpfr.6.dylib
     # /usr/lib/libz.1.dylib
-#    install_name_tool -id "homebrew:zstd/lib/libzstd.1.dylib" lib/libzstd.1.dylib
+    if [ "${ARCH}" = "x86_64" ]; then
+        install_name_tool -id "homebrew:zstd/lib/libzstd.1.dylib" lib/libzstd.1.dylib
+    fi
 #    install_name_tool -id "homebrew:gmp/lib/libgmp.10.dylib" lib/libgmp.10.dylib
 #    install_name_tool -id "homebrew:gcc/lib/gcc/current/libgcc_s.1.1.dylib" lib/libgcc_s.1.1.dylib
 fi
@@ -163,7 +168,9 @@ install_name_tool -change ${HOMEBREWDIR}/Cellar/nettle/${NETTLEVERSION}/lib/libn
     install_name_tool -change ${HOMEBREWDIR}/opt/mpfr/lib/libmpfr.6.dylib @executable_path/lib/libmpfr.6.dylib lib/libgccjit.0.dylib
     install_name_tool -change ${HOMEBREWDIR}/opt/gmp/lib/libgmp.10.dylib @executable_path/lib/libgmp.10.dylib lib/libgccjit.0.dylib
     # /usr/lib/libz.1.dylib
-    # install_name_tool -change ${HOMEBREWDIR}/opt/zstd/lib/libzstd.1.dylib @executable_path/lib/libzstd.1.dylib lib/libgccjit.0.dylib
+    if [ "${ARCH}" = "x86_64" ]; then
+        install_name_tool -change ${HOMEBREWDIR}/opt/zstd/lib/libzstd.1.dylib @executable_path/lib/libzstd.1.dylib lib/libgccjit.0.dylib
+    fi
     # otool -L lib/libisl.23.dylib
     install_name_tool -change ${HOMEBREWDIR}/opt/gmp/lib/libgmp.10.dylib @executable_path/lib/libgmp.10.dylib lib/libisl.23.dylib
     # otool -L lib/libmpc.3.dylib
