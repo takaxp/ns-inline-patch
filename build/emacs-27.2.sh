c6e70aa12ec19a1a2cbd5ed2d1340cfa4e14e420
Takaaki ISHIKAWA <takaxp@ieee.org>
2025-03-15 11:00:54 +0900

    Update scripts to use the latest release versions

--- build/emacs-27.2.sh
+++ build/emacs-27.2.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+VERSION=27.2
+
 # LIBXML2 for Catalina
 MACSDK=`xcrun --show-sdk-path`
 export LIBXML2_CFLAGS="-I${MACSDK}/usr/include/libxml2"
@@ -17,15 +19,23 @@ done
 
 cd ${WORKING_DIR}
 
-# Please select emacs-mirror if you have any connection troubles.
-# git clone --depth 1 https://github.com/emacs-mirror/emacs.git
-git clone git://git.sv.gnu.org/emacs.git
+# if [ ! -f emacs-$VERSION.tar.gz ]; then
+#     curl -LO ftp://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.gz
+# fi
+curl -LO ftp://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.gz
+
+rm -rf ./emacs-${VERSION}
+tar xvf ./emacs-$VERSION.tar.gz
+if [ ! -d "emacs-$VERSION" ]; then
+    echo "missing emacs-$VERSION/"
+    exit 1
+fi
+echo "---------------------------------"
 
 # inline-patch
 git clone --depth 1 https://github.com/takaxp/ns-inline-patch.git
 
-cd emacs
-git checkout --track origin/emacs-27
+cd emacs-${VERSION}
 patch -p1 < ../ns-inline-patch/emacs-27.1-inline.patch
 if [ $? -ne 0 ]; then echo "FAILED"; exit 1; fi
 # patch -p1 < ../ns-inline-patch/revert-89d0c445.patch
